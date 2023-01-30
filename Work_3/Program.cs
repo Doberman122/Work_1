935cd09f7935eb355d51331af1c9f545c4f9d2d8
Ruslan <hshsbsb062@gmail.com>
2023-01-30 20:27:36 +0300

    Work_3

---
+++ Work_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+string strValue = Console.ReadLine(); // Вводим число с клавиатуры, иными словами вводит пользователь.
+string strValue1 = Console.ReadLine(); 
+int value = Convert.ToInt32(strValue); // Конвертирование "Стринг" в свойство int 
+int value1 = Convert.ToInt32(strValue1);
+if (value1 > value) // Условие value > value1 
+{
+Console.WriteLine("Max " + value1); // Вывод max
+Console.WriteLine("Min " + value);
+}
+else
+{
+Console.WriteLine("Max " + value);
+Console.WriteLine("Min " + value1); // Вывод min
+}
+
