0368049b4c00db6b03c0b62c56c1ef4034e1f31c
Ruslan <hshsbsb062@gmail.com>
2023-01-30 20:03:58 +0300

    Work_1

---
+++ Work_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Нахождение, "Високосный год"
+
+Console.Write("Введите год: ");  
+int year = int.Parse(Console.ReadLine()); // Вод года c клавиатуры. 
+string output;
+
+if (year % 4 == 0) // проверка условия
+{
+    if (year % 100 == 0)
+    {
+        if (year % 400 == 0)
+        output = "високосный";
+        else
+        output = "невисокосный";
+    }
+    else
+    output = "високосный";
+}
+else
+output = "невисокосный";
+System.Console.WriteLine($"Год {year} - {output}");
