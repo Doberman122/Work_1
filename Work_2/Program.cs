4241f30b64dba3f10dde0458a4ef4effa06bbd80
Ruslan <hshsbsb062@gmail.com>
2023-01-30 20:21:14 +0300

    Work_2

---
+++ Work_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определение максимума, использование массивов и условий. 
+
+int max = 0;
+Console.Write("Введите три целых числа: ");
+string[] arr = Console.ReadLine().Split(); 
+
+int a = int.Parse(arr[0]);
+int b = int.Parse(arr[1]);
+int c = int.Parse(arr[2]);
+
+if (a >= b && a >= c)
+max = a;
+
+if (b >= a && b >= c)
+max = b;
+
+if (c >= a && c >= b)
+max = c;
+
+System.Console.WriteLine($"Максимум: {max}");
