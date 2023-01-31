719826172ebb73575d1afcc275409d6a902ae9f3
Ruslan <hshsbsb062@gmail.com>
2023-01-31 20:34:06 +0300

    Work_8

---
+++ Work_8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вычисление фактоиала
+
+ulong n = 0;
+ulong factorial = 1;
+
+System.Console.WriteLine("Введите положительное целое число");
+n = Convert.ToUInt64(Console.ReadLine());
+
+for (ulong i = 1; i <= n; i++)
+{
+    factorial *= i;
+}
+System.Console.WriteLine($"Факториал = {factorial}");
