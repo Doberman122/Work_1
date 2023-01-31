a18b1fde551f44d2b8914445d590faa0e4a78fca
Ruslan <hshsbsb062@gmail.com>
2023-01-31 20:52:34 +0300

    Work_9

---
+++ Work_9/Program.cs
@@ -0,0 +1,18 @@
+﻿int n1, n2, LCM= 0;
+Console.Write("Введите два целых числа: ");
+string[] arr = Console.ReadLine().Split();
+
+n1 = int.Parse(arr[0]);
+n2 = int.Parse(arr[1]);
+LCM = (n1 > n2) ? n1 : n2;
+
+// Бесконечный цикл
+while (true)
+{
+    if (LCM % n1 == 0 && LCM % n2 ==0)
+    {
+        Console.WriteLine($"HOK = {LCM}");
+        break;
+    }
+    ++LCM;
+}
